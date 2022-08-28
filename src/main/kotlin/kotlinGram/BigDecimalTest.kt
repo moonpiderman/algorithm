c084ce7b4ada92ebf466be141c18e9071938cba3
moonpiderman <lotsofchuck@gmail.com>
2022-08-28 13:26:47 +0900

    Algo

---
+++ src/main/kotlin/kotlinGram/BigDecimalTest.kt
@@ -0,0 +1,13 @@
+package kotlinGram
+
+import java.math.BigDecimal
+
+fun main() {
+    val notionUrl = "notion.site/"
+    val userUrl = "https://eggplant-sumac-51e.notion.site/c6f6b8f1b7574556a2540774e0d8aef2"
+
+    println(userUrl.contains(notionUrl))
+}
+
+class BigDecimalTest {
+}
