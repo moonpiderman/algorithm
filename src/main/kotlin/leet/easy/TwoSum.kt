549068abfb7a23b5f1a4a2278ea0c423331a7b30
moonpiderman <lotsofchuck@gmail.com>
2022-11-01 17:22:23 +0900

    Two Sum

--- src/main/kotlin/leet/easy/TwoSum.kt
+++ src/main/kotlin/leet/easy/TwoSum.kt
@@ -5,4 +5,14 @@ fun main() {
 }
 
 class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val visited = mutableMapOf<Int, Int>()
+        nums.forEachIndexed { index, num ->
+            if (visited.containsKey(num)) {
+                return intArrayOf(visited[num]!!, index)
+            }
+            visited[target - num] = index
+        }
+        return intArrayOf()
+    }
 }
