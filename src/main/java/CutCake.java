0d3db45fe47313c1be089fdfe6f2b1888f484305
moonpiderman <lotsofchuck@gmail.com>
2024-06-04 00:31:01 +0900

    케이크 자르기

---
+++ src/main/java/CutCake.java
@@ -0,0 +1,42 @@
+public class CutCake {
+    public static void main(String[] args) {
+        CutCake cutCake = new CutCake();
+
+        int[] topping1 = {1, 2, 1, 3, 1, 4, 1, 2};
+        int[] topping2 = {1, 2, 3, 1, 4};
+
+        System.out.println(cutCake.solution(topping1));
+        System.out.println(cutCake.solution(topping2));
+    }
+
+    public int solution(int[] topping) {
+        int maxTopping = 10000;
+        int[] leftToppings = new int[maxTopping + 1];
+        int[] rightToppings = new int[maxTopping + 1];
+        int leftUnique = 0;
+        int rightUnique = 0;
+
+        int fairCutCount = 0;
+
+        for (int t : topping) {
+            if (rightToppings[t]++ == 0) {
+                rightUnique++;
+            }
+        }
+
+        for (int i = 0; i < topping.length - 1; i++) {
+            if (leftToppings[topping[i]]++ == 0) {
+                leftUnique++;
+            }
+            if (--rightToppings[topping[i]] == 0) {
+                rightUnique--;
+            }
+
+            if (leftUnique == rightUnique) {
+                fairCutCount++;
+            }
+        }
+
+        return fairCutCount;
+    }
+}
