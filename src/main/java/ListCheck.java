da584b2c3494193c7fe24fb47170e97e5658b796
moonpiderman <lotsofchuck@gmail.com>
2024-07-09 10:45:55 +0900

    list check

---
+++ src/main/java/ListCheck.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListCheck {
+    public static void main(String[] args) {
+
+    }
+
+    public void solution() {
+        ArrayList<String> arrayList = new ArrayList<>();
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        System.out.println();
+    }
+}
